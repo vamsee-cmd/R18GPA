43d3a15835eb040fe3a1475d807af964db6a489e
vamsee-cmd <vamshi.embadi1234@gmail.com>
2022-01-17 11:31:17 +0530

    adsense linked

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 <head><title>Gpa calculator</title>
  <link href="styles.css" rel="stylesheet" />
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7754624932884765" crossorigin="anonymous"></script>
+</head>
+<body>
  <center>
 <div class="container justify-content-center">
     <br>
